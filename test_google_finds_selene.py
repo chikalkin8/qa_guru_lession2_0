0233981070dfc2ba7fa7ace80fe5a3a43a6c357e
chikalkin8 <drobiazka@yandex.ru>
2022-09-08 18:47:51 +0300

    fixture

--- test_google_finds_selene.py
+++ test_google_finds_selene.py
@@ -1,6 +1,22 @@
+import pytest
 from selene.support.shared import browser
 from selene import have, be
 
-browser.open('https://google.com')
-browser.element('[name=q]').should(be.blank).type('selene').press_enter()
-browser.element('[id=search]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+
+@pytest.fixture()
+def configure_size_window_browser():
+    browser.config.window_width = 300
+    browser.config.window_height = 700
+
+
+def test_positive(configure_size_window_browser):
+    browser.open('https://google.com')
+    browser.element('[name=q]').should(be.blank).type('selene').press_enter()
+    browser.element('[id=search]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+
+
+def test_negative():
+    browser.open('https://google.com')
+    browser.element('[name=q]').should(be.blank).type('selene').press_enter()
+    browser.element('[id=search]').should(have.no.text('Selene: User-oriented Web UI browser tests in Python'))
+
